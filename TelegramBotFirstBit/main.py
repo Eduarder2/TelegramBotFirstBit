ab301a81fb4b8fe4a2aa124b8c4ed5c1dee99bfb
Eduard Eritsyan <Eritsian.EZh@phystech.edu>
2022-04-05 13:36:08 +0300

    Добавьте файлы проекта.

---
+++ TelegramBotFirstBit/main.py
@@ -0,0 +1,82 @@
+﻿
+
+import telebot
+import positions
+
+# Username пользователей, взаимодействующих с ботом
+active_users = {
+    'manager': [],
+    'implementer': [],
+    'coordinator': [],
+    'administrator': []
+    }
+
+# Нужно определиться, где будем хранить БД
+PATH_ID_POSITION_NAME = 'C:\\Users\\Эдуард\\Desktop\\1.txt'
+
+try:
+    with open('TOKEN.txt') as token:
+        TOKEN = token.readline()
+except FileNotFoundError:
+    raise FileNotFoundError('A token of bot not found.')
+
+try:
+    with open(PATH_ID_POSITION_NAME, encoding='UTF-8') as employees:
+        id_position_name = dict()
+        for emp in employees:
+            lst = emp.split('\t')
+            id_position_name[lst[0]] = tuple(lst[1:])
+except FileNotFoundError:
+    with open(PATH_ID_POSITION_NAME, 'wt') as employees:
+        pass
+
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def def_position(message):
+    ''' Define position of employee (manager, admin, ...) '''
+
+    id_ = str(message.chat.id)
+    if not id_ in id_position_name.keys():
+        bot.send_message(id_, text='Вы не числитесь сотрудником компании. ' +\
+                         'Для уточнения информации обратитесь к координатору.')
+    else:
+
+        # Необходимо добавить кнопку "Приступим!"
+
+        bot.send_message(id_, 'Привет, ' + id_position_name[id_][1])
+        pos = id_position_name[id_][0]
+        name = id_position_name[id_][1]
+        if pos == 'manager':
+            active_users[pos].append({id_: positions.Manager(name, id_)})
+        elif pos == 'implementer':
+            active_users[pos].append({id_: positions.Implementer(name, id_)})
+        elif pos == 'coordinator':
+            active_users[pos].append({id_: positions.Coordinator(name, id_)})
+        elif pos == 'administrator':
+            active_users[pos].append({id_: positions.Administrator(name, id_)})
+        else:
+            pass # Написать обработку исключений
+
+    
+@bot.message_handler(content_types=['text'])
+def process_order(message):
+    id_ = str(message.chat.id)
+    pos = id_position_name[id_][0]
+    if pos == 'manager':
+        if message.text == 'Приступим!':
+            active_users[pos][id_].send_message_to_coordinator()
+    elif pos == 'implementer':
+        pass
+    elif pos == 'coordinator':
+        pass
+    elif pos == 'administrator':
+        pass
+    else:
+        pass 
+
+
+
+
+bot.infinity_polling()
---
+++ TelegramBotFirstBit/positions.py
@@ -0,0 +1,42 @@
+﻿
+
+import telebot
+
+class Manager(telebot.TeleBot):
+    
+    def __init__(self, name, id_):
+        self.name = name
+        self.id = id_
+
+    def get_number_of_order(self):
+        pass
+
+    def get_clients_name(self):
+        pass
+
+    def get_phone(self):
+        pass
+
+    def get_problem(self):
+        pass
+
+    def get_scheduled_date(self):
+        pass
+
+    def send_message_to_coordinator(self):
+        self.get_number_of_order()
+        self.get_clients_name()
+        self.get_phone()
+        self.get_problem()
+        self.get_scheduled_date()
+
+
+        
+class Implementer(telebot.TeleBot):
+    pass
+
+class Coordinator(telebot.TeleBot):
+    pass
+
+class Administrator(telebot.TeleBot):
+    pass
